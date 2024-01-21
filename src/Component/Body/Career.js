301e3b52503d1781d6169314eff94636185b8e9f
shubhamdalvi <global-config>
2024-01-21 12:44:23 +0530

    update

--- src/Component/Body/Career.js
+++ src/Component/Body/Career.js
@@ -1,8 +1,103 @@
 
 
-const Career = () =>{
-    return(
-        <h1>I am Career Page</h1>
+const Career = () => {
+    return (
+        <>
+            <div className="container-fluid rohit">
+                <div className="container pt-5 pb-5">
+                    <h1 className="text-center pt-5 fw-bold text-warning" style={{ fontSize: "55px" }}>Work With Us</h1>
+                    <p className="text-center pt-2 fw-bold  text-warning" style={{ fontSize: "15px" }}>We are a Francisco-based startup & We are ready for new talent to join our company.</p>
+                    <div className="text-center pt-5">
+                        <button className="btn btn-outline-primary px-5 fw-bold  rounded-5" style={{ fontSize: "20px" }}>Get Started</button>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            <div className="container pt-5 pb-5 ">
+                <h1 className="text-center">About Working Here</h1>
+                <div className="mx-4 w-100" style={{maxWidth:"1800px"}} >
+                    <div className="justify-content-center shubham">
+                        <div className="row ">
+                            <div className="w-50">
+                                <div className="pt-5 pb-5 d-flex align-items-center">
+                                    <span className="material-symbols-outlined text-primary  " style={{ fontSize: "70px" }}>
+                                        emoji_events
+                                    </span>
+                                    <div className="w-50 mx-4">
+                                        <p className="fw-bold  mb-1 ">Conductive Environment</p>
+                                        <p className="mt-0">Enjoy challenging work and satisfying job without any hierarchy</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" w-50">
+                                <div className="pt-5 pb-5 d-flex align-items-center">
+                                    <span className="material-symbols-outlined text-primary" style={{ fontSize: "70px" }}>
+                                        database
+                                    </span>
+                                    <div className="w-50 mx-4">
+                                        <p className="fw-bold  mb-1">Shared Success</p>
+                                        <p className=" mt-0"    >Equity ownership & opportunity await</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row ">
+                            <div className="col-6 w-50">
+                                <div className=" pb-5 d-flex align-items-center">
+                                    <span className="material-symbols-outlined text-primary" style={{ fontSize: "70px" }}>
+                                        tapas
+                                    </span>
+                                    <div className="w-50 mx-4">
+                                        <p className="fw-bold mb-1">Healthy Meals & Snacks</p>
+                                        <p className=" mt-0">Enjoy challenging work and satisfying job without any hierarchy</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6 w-50">
+                                <div className=" pb-5 d-flex align-items-center">
+                                    <span className="material-symbols-outlined text-primary" style={{ fontSize: "70px" }}>
+                                        medical_services
+                                    </span>
+                                    <div className="w-50 mx-4">
+                                        <p className="fw-bold mb-1">Medical Insuarance</p>
+                                        <p className=" mt-0">We have got you covered, literally!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row ">
+                            <div className="col-6 w-50">
+                                <div className=" pb-5 d-flex align-items-center">
+                                    <span className="material-symbols-outlined text-primary" style={{ fontSize: "70px" }}>
+                                        devices_other
+                                    </span>
+                                    <div className="w-50 mx-4">
+                                        <p className="fw-bold mb-1">Best Gadgets</p>
+                                        <p className=" mt-0">Pamper the tech savvyin you with gadgetand advanced technology</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6 w-50">
+                                <div className=" pb-5 d-flex align-items-center">
+                                    <span className="material-symbols-outlined text-primary" style={{ fontSize: "70px" }}>
+                                        medical_services
+                                    </span>
+                                    <div className="w-50 mx-4">
+                                        <p className="fw-bold mb-1">Cultural of Learning</p>
+                                        <p className=" mt-0">Regular brown bag lunches to keep the brain rejuvented</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/Component/Body/Contact.js
+++ src/Component/Body/Contact.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 
 const Contact = () =>{
@@ -6,15 +5,15 @@ const Contact = () =>{
     
     return(
         <>
-        <div className="d-flex p-5">
+        <div className="d-flex p-5 contactMain" >
             <div className="p-2 w-50">
                 <div className="">
                     <h2 className="text-center">Get in touch</h2>
                 </div>
-                <form className=""> 
-                    <div className="d-flex align-items-center justify-content-evenly mt-4">
-                    <input type="text"  placeholder="Name" className="p-2 fs-5 rounded"></input>
-                    <input type="email" placeholder="Email" className="p-2 fs-5 rounded"></input>
+                <form> 
+                    <div className="d-flex align-items-center justify-content-around mt-4">
+                    <input type="text"  placeholder="Name" className="p-2 fs-5 rounded" style={{width:"37%"}}></input>
+                    <input type="email" placeholder="Email" className="p-2 fs-5 rounded" style={{width:"37%"}}></input>
                     </div>
                     <div className="p-4 mt-1" style={{marginLeft:"12px"}}>
                     <input type="text" placeholder="Subject" className="p-2 fs-5 rounded" style={{width:"98%"}}></input>
@@ -24,29 +23,30 @@ const Contact = () =>{
                     <button className="p-3 rounded text-black fs-6" style={{margin:"2% 5%", border:"none"}}>Send Message</button>
                 </form>
             </div>
-            <div className="w-50 bg-secondary p-2 text-center">
-                <h2>Contact US</h2>
-
+            <div className="w-50 p-2 text-center">
+                <h2 className="mt-2">Contact US</h2>
+<div className="m-3">
                 <div className="d-flex p-3 align-items-center fs-3 m-auto" style={{width:"70%", gap:"10px"}}>
-                <i class="fa-solid fa-location-dot fa-xl" style={{color: "#fcfcfd"}}></i>
+                <i class="fa-solid fa-location-dot fa-sm" style={{color: "#fcfcfd"}}></i>
                 <h5 className="fs-5">Address :  198 West 21th Street, Suite 721 New York NY 10016</h5>    
                 </div>
 
                 <div className="d-flex p-3 align-items-center fs-3 m-auto" style={{width:"70%", gap:"10px"}}>
-                <i class="fa-light fa-phone fa-xl" style={{color: "#fcfcfd"}}></i>
-                <h5 className="fs-5">Phone :  </h5>    
+                <i class="fa-light fa-mobile fa-xl" style={{color: "#fcfcfd"}}></i>
+                <h5 className="fs-5 mx-3">Phone : 834656846 </h5>    
                 </div>
 
 
                 <div className="d-flex p-3 align-items-center fs-3 m-auto" style={{width:"70%", gap:"10px"}}>
-                <i class="fa-solid fa-location-dot fa-xl" style={{color: "#fcfcfd"}}></i>
-                <h5 className="fs-5">Address : </h5>    
+                <i class="fa-solid fa-paper-plane fa-sm" style={{color: "#fcfcfd"}}></i>
+                <h5 className="fs-5 mx-3">Email : info@yoursite.com</h5>    
                 </div>
 
                 <div className="d-flex p-3 align-items-center fs-3 m-auto" style={{width:"70%", gap:"10px"}}>
-                <i class="fa-solid fa-location-dot fa-xl" style={{color: "#fcfcfd"}}></i>
-                <h5 className="fs-5">Address : </h5>    
+                <i class="fa-solid fa-globe fa-sm" style={{color: "#fcfcfd"}}></i>
+                <h5 className="fs-5 mx-3">Website : yoursite.com </h5>    
                 </div>
+            </div>
             </div>
         </div>
         </>
